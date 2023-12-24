f971d34eaff1a7532113124f3d6cc42377acc6ea
aafreen-ansari05 <aafreenjaved10@gmail.com>
2023-12-24 11:50:35 +0530

    code

---
+++ area_perimeter_of_circle.js
@@ -0,0 +1,11 @@
+/**
+ * finding area and perimeter of circle
+ * @param {Int} r
+ * return {Int}
+ */
+function AreaandPerimeter(r){
+    console.log("Area : "+(3.142*r*r)+ "  " +("Perimeter : ")+(6.284*r)) ;
+}
+
+let b = AreaandPerimeter(4);
+ 
