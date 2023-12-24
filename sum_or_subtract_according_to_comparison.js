43c758ab5a9708b367e8f8db7244efd426ac6e07
aafreen-ansari05 <aafreenjaved10@gmail.com>
2023-12-24 13:47:39 +0530

    initial commit

---
+++ sum_or_subtract_according_to_comparison.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {Int} a 
+ * @param {Int} b 
+ * if a is greater than b then function returns (a-b) otherwise, it will return (a+b)----
+ * a and b should be Number or integer----
+ */
+function sumSubtact(a,b){
+    if(a>b){
+        console.log(a-b);
+    }
+    else{
+    console.log(a+b);
+    }
+}
+let f = sumSubtact(40,20);
