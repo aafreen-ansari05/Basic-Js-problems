701df9dcca797b444dc84add1c9effdd6b0b0b7f
mailtodanish <mailtodanish@gmail.com>
2023-12-24 12:23:36 +0530

    review

--- area_perimeter_of_circle.js
+++ area_perimeter_of_circle.js
@@ -2,6 +2,10 @@
  * finding area and perimeter of circle
  * @param {Int} r
  * return {Int}
+ * TODO:
+ *  check data type inside function
+ *  return proper value from function
+ *  functiuon name will be in Camel Case and start with VERB [calculateAreaAndParameter]
  */
 function AreaandPerimeter(r){
     console.log("Area : "+(3.142*r*r)+ "  " +("Perimeter : ")+(6.284*r)) ;
