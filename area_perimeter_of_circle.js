a2c30b83a92a8cf959ce6a9e3550e295b4855f1e
aafreen-ansari05 <aafreenjaved10@gmail.com>
2023-12-24 12:49:58 +0530

    code

--- area_perimeter_of_circle.js
+++ area_perimeter_of_circle.js
@@ -1,15 +1,16 @@
 /**
  * finding area and perimeter of circle
  * @param {Int} r
- * return {Int}
+ *  return {Int}
  * TODO:
  *  check data type inside function
  *  return proper value from function
  *  functiuon name will be in Camel Case and start with VERB [calculateAreaAndParameter]
  */
 function AreaandPerimeter(r){
-    console.log("Area : "+(3.142*r*r)+ "  " +("Perimeter : ")+(6.284*r)) ;
+    return "Area : "+(3.142*r*r)+ "  " +("Perimeter : ")+(6.284*r)
 }
 
 let b = AreaandPerimeter(4);
+console.log(b) ;
  
