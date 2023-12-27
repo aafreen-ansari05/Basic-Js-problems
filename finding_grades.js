5cdfafb6c1a8ee716bd16e4fb5235cb5601a0a7b
aafreen-ansari05 <aafreenjaved10@gmail.com>
2023-12-27 12:23:43 +0530

    initial commit

---
+++ finding_grades.js
@@ -0,0 +1,14 @@
+let markRohan = 50;
+let markSohan = 60;
+let markMohan = 70;
+let markGopal = 80;
+function grade(){
+    if(85-100)  console.log("A");
+    else if(70-84) console.log("B");
+    else if(55-69) console.log("C")
+    else if(45-54)  console.log("D");
+    else  console.log("E");
+}
+grade();
+
+
