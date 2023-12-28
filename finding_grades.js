d5e8d6b5e5e6de12bdd7f422174a62470f2e6d6a
aafreen-ansari05 <aafreenjaved10@gmail.com>
2023-12-28 10:20:58 +0530

    updated

--- finding_grades.js
+++ finding_grades.js
@@ -5,35 +5,16 @@ let markRohan = 90;                         //first student
 let markSohan = 60;                         //second student
 let markMohan = 70;                         //third student
 let markGopal = 80;                         //fourth student
-
-/**
- *  Decsription - 
- * @param {Number} marks 
- */
 function grade1(marks){
-    // if(marks > 85 && marks < 100)  console.log("grade : A");
-    // else if(marks > 70 && marks < 84)console.log("grade : B");
-    // else if(marks > 55 && marks < 69)console.log("grade : C")
-    // else if(marks > 45 && marks < 54)console.log("grade : D");
-    // else  console.log("grade : E");
-
-    let grade = "E"
-    switch(marks){
-        case marks > 85 && marks < 100:
-            grade =  "A"
-            break
-
-        case marks > 70 && marks < 84:
-            grade =  "A"
-            break
-    }
-
-    return grade;
+    if(marks > 85 && marks < 100)  console.log("grade : A");
+    else if(marks > 70 && marks < 84)console.log("grade : B");
+    else if(marks > 55 && marks < 69)console.log("grade : C")
+    else if(marks > 45 && marks < 54)console.log("grade : D");
+    else  console.log("grade : E");
 }
 grade1(markRohan);
 grade1(markSohan);
 grade1(markMohan);
-let g = grade1(markGopal);
-console.error(g)
+grade1(markGopal);
 
 
