d0fed5751fbda4c3d3ac2b4128fc2f6e32e36bf7
aafreen-ansari05 <aafreenjaved10@gmail.com>
2024-01-01 12:30:43 +0530

    initial commit

---
+++ 1_valid_date.js
@@ -0,0 +1,13 @@
+const Date = [234];
+const Month = [2];
+const Year = [2024];
+if(Date>31){
+    console.log("Your date is not valid")
+}
+if(Month>12){
+    console.log("Your month is not valid")
+}
+if(Year>2023){
+    console.log("Your year is not valid")
+}
+
