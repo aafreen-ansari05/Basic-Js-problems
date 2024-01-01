ea5ee62abb0139292ece2e79fe03ccbd6b085ecb
aafreen-ansari05 <aafreenjaved10@gmail.com>
2024-01-01 08:03:35 +0530

    initial commit

---
+++ 31_vowel_or_consonent.js
@@ -0,0 +1,14 @@
+const letter = "x";
+switch(letter){
+    case "a": console.log( "This is a vowel.");
+        break;
+    case "e": console.log( "This is a vowel.");
+        break;
+    case "i": console.log( "This is a vowel.");
+        break;
+    case "o": console.log( "This is a vowel.");
+        break;
+    case "u": console.log( "This is a vowel.");
+         break;
+    default : console.log( "This is a consonent.");
+}
